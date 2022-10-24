19e0bdad93e189c8a84a1ba17f460b7221738f76
kokakoder <116548627+kokakoder@users.noreply.github.com>
2022-10-24 09:06:10 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,50 @@
+basic.showString("Hello!")
+basic.showString("my name is sean ")
+basic.showString("how old are you")
+basic.showIcon(IconNames.Happy)
+basic.showString("what is your name")
+basic.showString("do you want to be friends")
+basic.clearScreen()
+basic.showLeds(`
+    . . # . .
+    . # . # .
+    . # # # .
+    . # # # .
+    . # . # .
+    `)
+basic.showLeds(`
+    . # . # .
+    . # # # .
+    . # # # .
+    . # . # .
+    . . . . .
+    `)
+basic.showLeds(`
+    . # # # .
+    . # # # .
+    . # . # .
+    . . . . .
+    . . . . .
+    `)
+basic.showLeds(`
+    . # # # .
+    . # . # .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.showLeds(`
+    . # . # .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.clearScreen()
+basic.showString("do you want to have a playdate")
+basic.showString("hi when is your bithday")
+basic.showString("can we go to adrenalin together")
+basic.showString("I am very exited")
+basic.forever(function () {
+	
+})
